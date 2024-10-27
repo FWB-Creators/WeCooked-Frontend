6f595946e90d72428bd2fb5cfe17f3eb84659089
Tongkungza <tongs2004@gmail.com>
2024-10-28 02:21:39 +0700

    add use context

--- app/client/components/NavbarBefore.tsx
+++ app/client/components/NavbarBefore.tsx
@@ -1,14 +1,15 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid';
-import Link from 'next/link';
-import Image from 'next/image';
-import NavLink from './NavLink';
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+import Link from 'next/link'
+import Image from 'next/image'
+import NavLink from './NavLink'
+import { useContext } from 'react'
 
 export default function NavbarBefore() {
   return (
     <nav className="flex items-center justify-between bg-white py-6 shadow-xl">
       <div className="px-12">
         <Link href="/">
-          <Image 
+          <Image
             src="/svg/WecookedLogo.svg"
             alt="WeCooked Logo"
             width={120}
@@ -24,18 +25,18 @@ export default function NavbarBefore() {
         <NavLink href="/client/group">Group</NavLink>
         <NavLink href="/client/private">Private</NavLink>
       </div>
-        <div className="relative">
-          <input
-            type="text"
-            aria-label="Search"
-            className="border border-[#FE3511] rounded-lg w-[530px] pl-10 pr-3 outline-none"
-          />
-          <MagnifyingGlassIcon
-            className="h-6 w-6 absolute left-3 top-1/2 transform -translate-y-1/2 text-[#FE3511] pointer-events-none"
-            aria-hidden="true"
-          />
-        </div>
-        <div className="flex items-center px-6">
+      <div className="relative">
+        <input
+          type="text"
+          aria-label="Search"
+          className="border border-[#FE3511] rounded-lg w-[530px] pl-10 pr-3 outline-none"
+        />
+        <MagnifyingGlassIcon
+          className="h-6 w-6 absolute left-3 top-1/2 transform -translate-y-1/2 text-[#FE3511] pointer-events-none"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="flex items-center px-6">
         <NavLink href="/client/login">Login</NavLink>
         <NavLink href="/client/sign-up">Sign Up</NavLink>
       </div>
