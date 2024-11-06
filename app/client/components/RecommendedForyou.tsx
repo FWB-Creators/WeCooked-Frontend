8fd724d500cc06339c47643990128bf54b7a9c12
Sikares <sikares.n@gmail.com>
2024-11-07 03:19:49 +0700

    fix: RecommendedForYou Page

--- app/client/components/RecommendedForyou.tsx
+++ app/client/components/RecommendedForyou.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { courses } from '../data/recommended-for-you';
+import { courses } from "../data/recommended-for-you";
 import CourseCard from './CourseCard';
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
 
