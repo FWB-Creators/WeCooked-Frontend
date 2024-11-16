36c3c5b9a08d3046240fa957ab0e991af60c8039
Sikares <sikares.n@gmail.com>
2024-11-17 03:43:29 +0700

    feat: Chef Video/Group Content

---
+++ app/chef/group/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+import GroupTable from '../components/GroupTable'
+
+export default function Home() {
+  return (
+    <div className="animate-fadeIn">
+      <Navbar />
+      <GroupTable />
+    </div>
+  )
+}
---
+++ app/chef/video/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+import VideoTable from '../components/VideoTable'
+
+export default function Home() {
+  return (
+    <div className="animate-fadeIn">
+      <Navbar />
+      <VideoTable />
+    </div>
+  )
+}
