93a070a07aa73df9dbfa8d8c1160aa72eaeca43f
Sikares <sikares.n@gmail.com>
2024-11-27 02:35:59 +0700

    feat: GroupCard

---
+++ app/client/types/groupCard.d.ts
@@ -0,0 +1,11 @@
+export interface Group {
+  groupId: number
+  groupTitle: string
+  groupCategory: string
+  groupPrice: number
+  reviewRating: number
+  chefName: string
+  courseImage: string
+  chefImage: string
+  courseDietary?: string[]
+}
